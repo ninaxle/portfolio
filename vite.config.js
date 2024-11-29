1b9ac6a09ceefa1082e55d84d15752f8412a3d95
Felix Fong <fongf2@mcmaster.ca>
2024-11-29 02:41:55 -0500

    moved assets out

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "./src",
   build: {
-    outDir: "./src/dist", // Output build to a 'dist' folder in the root
+    outDir: "dist", // Output build to a 'dist' folder in the root
   },
   server: {
     port: 5173, // Port you want the dev server to run on
