d3edaee6fd9c8f13b0276d3cdab4fd863ad8fd52
Felix Fong <fongf2@mcmaster.ca>
2024-12-02 01:08:41 -0500

    refactor: created src folder and assets folder

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
 import { defineConfig } from "vite";
-import { resolve } from 'path';
+import { resolve } from "path";
 
 export default defineConfig({
+  root: "src",
   build: {
-    outDir: "dist", // Output build to a 'dist' folder in the root
+    outDir: "../dist", // Output build to a 'dist' folder in the root
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        archive: resolve(__dirname, 'archive.html'),
-        footer: resolve(__dirname, 'footer.html'),
-        navbar: resolve(__dirname, 'navbar.html'),
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        archive: resolve(__dirname, "archive.html"),
+        footer: resolve(__dirname, "footer.html"),
+        navbar: resolve(__dirname, "navbar.html"),
         // Add all your additional pages here
-      }
+      },
     },
   },
   server: {
     port: 5173, // Port you want the dev server to run on
   },
-  publicDir: "assets",
+  publicDir: "../assets",
 });
